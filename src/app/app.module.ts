b1c1ec573954ef583cfd21df7b1a3d6226a4fa86
solomon <solomonggichuru@gmail.com>
2020-09-23 08:42:40 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+     StrikethroughDirective,
+     GoalDetailComponent,
+     DateCountPipe
   ],
   imports: [
     BrowserModule,
